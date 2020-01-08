2776277e2ebcc6c09dc8ce73fc9a6308b6c2724b
Ebony Brown <ebony.ashley.brown@gmail.com>
2020-01-08 00:11:22 -0600

    Added 5 files and 2 images
    added background image, head shot, resume, script.js, index.html, resume, style.css

---
+++ indexplay.js
@@ -0,0 +1,8 @@
+const dogName = [
+    {name: jim, age: 18 },
+    {name: james, age: 16 },
+    {name: john, age: 24 },
+    {name: Tyron, age: 25}
+];
+
+dogName.forEach(dog => console.log(dog.name));
---
+++ script.js
@@ -0,0 +1,19 @@
+// // once skills button clicked pop over for skills displays
+// $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover();   
+// });
+
+// this function is invoked when resume button clicked and directs user to pdf of resume
+show = () => {
+    return(window.location.href="Regions resume (4).pdf");    
+}
+
+// this function is invoked when projects button clicked and directs you to projects page
+project = () => {
+    return(window.location.href="projects.html");    
+}
+
+// $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover();   
+// });
+
